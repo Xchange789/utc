57aea48dfc7f7295cef9de57cf806341385c40ae
Xchange789 <xchange.report@gmail.com>
2024-11-08 10:51:14 +0800

    111
    111

--- TelegramSDK.js
+++ TelegramSDK.js
@@ -1,15 +1,7 @@
 (function (){
     // receive event
     window.TelegramGameProxy.receiveEvent =
-        (eventType, eventData) => {
-            console.log(eventType);
-            console.log(eventData);
-        };
     window.Telegram.WebView.receiveEvent  =
-        (eventType, eventData) => {
-            console.log(eventType);
-            console.log(eventData);
-        };
     window.TelegramGameProxy_receiveEvent =
         (eventType, eventData) => {
             console.log(eventType);
